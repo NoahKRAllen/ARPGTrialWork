ee00123a36352fb9d7f36f41a21525f5ac1a08ca
Noah Allen <NoahKRAllen@gmail.com>
2021-12-01 14:21:30 -0800

    Finished camera control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    GameObject _playerCharacter;
+    Transform _playerCharacter;
 
-    Vector3 _distFromPlayer;
+    Vector3 _offset;
 
     public float _smoothSpeed = 0.125f;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        _playerCharacter = GameObject.FindGameObjectWithTag("Player");
-        _distFromPlayer = transform.position - _playerCharacter.transform.position;
+        _playerCharacter = GameObject.FindGameObjectWithTag("Player").transform;
+        _offset = transform.position - _playerCharacter.transform.position;
     }
 
     void LateUpdate()
     {
-        
+        Vector3 desiredPos = _playerCharacter.position + _offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, _smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPos; 
     }
 }
