4e78cf0d5c93aeecd2163d4a1492bfbc80a8b413
Noah Allen <NoahKRAllen@gmail.com>
2021-12-02 14:01:31 -0800

    Rough aim finished

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class PlayerAim : MonoBehaviour
 {
-    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        
     }
     void Update()
     {
-        Vector3 mouse = Input.mousePosition;
-        Vector3 mouseWorld = Camera.main.ScreenToViewportPoint(new Vector3(
-                                                            mouse.x,
-                                                            mouse.y,
-                                                            player.transform.position.y));
-        Vector3 forward = mouseWorld - player.transform.position;
-        player.transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(mouseRay, out hit))
+        {
+            if(hit.collider.tag == "Ground")
+                gameObject.transform.LookAt(hit.point);   
+        }
     }
 }
